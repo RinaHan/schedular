f827fdf8d3be7abb7b887b5a5a7cf48c670059c8
RinaHan <rinahan4146@gmail.com>
2021-07-26 18:18:54 -0700

    updated page layout

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
 import logo from "./logo.svg";
 import "./App.scss";
+// import Select from "./comps/Select";
 
+const Day = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 function App() {
   return (
     <div className="Container">
       <div className="Web">
-        <div className="Nav"></div>
+        <div className="Nav">❤️ Rina</div>
         <div className="Main">
           <div className="Header">Dashboard</div>
           <div className="Body">
             <div className="MainSec">
-              <div className="InfoCont"></div>
-              <div className="WeeklyCont"></div>
+              <div className="InfoCont">
+                <div className="InputCont">
+                  <label for="a">Title</label>
+                  <input id="a" type="text" placeholder="Title" />
+                </div>
+                <div className="InputCont">
+                  <label for="b">Date</label>
+                  <input id="b" type="text" placeholder="Day of the week" />
+                </div>
+                <div className="InputCont">
+                  <label for="c">Time</label>
+                  <input id="c" type="number" placeholder="Hours" />
+                </div>
+                {/* <Select/> */}
+              </div>
+              <div className="WeeklyCont">
+                {Day.map((o,i)=>{
+                  return(
+                    <div className="Day">{o}</div>
+                  )
+                })}
+              </div>
             </div>
             <div className="Aside"></div>
           </div>
