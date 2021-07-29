8cd30db6b752ffa68e5b57173b5209dda982b7c1
RinaHan <rinahan4146@gmail.com>
2021-07-28 22:47:37 -0700

    fixed buts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import logo from "./logo.svg";
 import "./App.scss";
-import DayComponent from "./comps/DayComponent";
+import DateComponent from "./comps/DateComponent";
 // import Select from "./comps/Select";
 
-const Day = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+const Date = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 function App() {
   return (
     <div className="Container">
@@ -12,31 +12,40 @@ function App() {
         <div className="Main">
           <div className="Header">Dashboard</div>
           <div className="Body">
+
             <div className="MainSec">
+
               <div className="InfoCont">
                 <div className="InputCont">
                   <label for="a">Title</label>
                   <input id="a" type="text" placeholder="Title" />
                 </div>
                 <div className="InputCont">
-                  {/* <label for="b">Date</label>
-                  <input id="b" type="text" placeholder="Day of the week" /> */}
-                <DayComponent/>
+                  <DateComponent />
                 </div>
                 <div className="InputCont">
                   <label for="c">Time</label>
                   <input id="c" type="number" placeholder="Hours" />
                 </div>
+                <button className="ButtonCont"
+              onClick={()=>{
+                
+              }}
+                >
+                Create the schedule
+                </button>
               </div>
+
               <div className="WeeklyCont">
-                {Day.map((o,i)=>{
-                  return(
-                    <div className="Day">{o}</div>
-                  )
+                {Date.map((o, i) => {
+                  return <div className="Date">{o}</div>;
                 })}
               </div>
+
             </div>
+
             <div className="Aside"></div>
+
           </div>
         </div>
       </div>
