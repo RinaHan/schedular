59275ed74442eb032fa618e18a95d908d54c0cdf
RinaHan <rinahan4146@gmail.com>
2021-07-28 21:18:42 -0700

    created Daycomps with selected state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.scss";
+import DayComponent from "./comps/DayComponent";
 // import Select from "./comps/Select";
 
 const Day = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
@@ -18,14 +19,14 @@ function App() {
                   <input id="a" type="text" placeholder="Title" />
                 </div>
                 <div className="InputCont">
-                  <label for="b">Date</label>
-                  <input id="b" type="text" placeholder="Day of the week" />
+                  {/* <label for="b">Date</label>
+                  <input id="b" type="text" placeholder="Day of the week" /> */}
+                <DayComponent/>
                 </div>
                 <div className="InputCont">
                   <label for="c">Time</label>
                   <input id="c" type="number" placeholder="Hours" />
                 </div>
-                {/* <Select/> */}
               </div>
               <div className="WeeklyCont">
                 {Day.map((o,i)=>{
